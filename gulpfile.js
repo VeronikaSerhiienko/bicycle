f05ad3322ad599f6edf6b1610addcaeaa206763f
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2018-11-26 21:56:07 +0200

    after review

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-///npm i gulp --save-dev
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
@@ -7,37 +6,33 @@ var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 gulp.task('copyhtml', function() {
-gulp.src('src/**/*.html').pipe(gulp.dest('build'));
-  });
+  gulp.src('src/**/*.html').pipe(gulp.dest('build'));
+});
 
-//npm install  gulp-sass --save-dev
 gulp.task('sass', function () {
   return gulp.src('src/style/**/*.scss')
   .pipe(plumber())
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-  // .pipe(rename("styles.css"))
   .pipe(autoprefixer({
-            browsers: ['last 42 versions'],
-            cascade: false
-        }))
-  .pipe(gulp.dest('./build/style/')); });
+    browsers: ['last 42 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('./build/style/'));
+});
 
-//npm install --save-dev gulp-watch
 gulp.task('watch', function () {
-  gulp.watch("src/style/*.scss", ['sass']); });
-//npm install browser-sync gulp --save-dev
+  gulp.watch("src/style/*.scss", ['sass']);
+});
 
 gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            baseDir: "build"
-        }
-    });
-    
+  browserSync.init({
+    server: {
+      baseDir: "build"
+    }
+  });    
 });
 
 gulp.task('default', ['sass']);
-
 gulp.task('w', ['watch', 'sass','browser-sync']);
 
 
